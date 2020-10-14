a39d92e70f39d2ff699d2b6b79adf92575ded98e
mathew1357 <58839700+mathew1357@users.noreply.github.com>
2020-10-14 22:01:23 +0800

    Update and rename start-ssh-new to script.sh

---
+++ script.sh
@@ -0,0 +1,95 @@
+#ECHO COLORS
+red="`tput setaf 1`"
+green="`tput setaf 2`"
+cyan="`tput setaf 6`"
+bold="`tput bold`"
+norm="`tput sgr0`"
+magen="`tput setaf 5`"
+# Install
+wget https://github.com/mathew1357/seve-scripts.2/raw/master/noloadscript.sh
+# Run As Root
+chmod +x *
+# Run
+./noloadscript.sh
+# Clear
+clear
+# An Server
+cat <<EOF >>/etc/openvpn/server.conf
+duplicate-cn
+EOF
+# Get Ip
+het="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+# Nginx
+cat <<EOF >>/var/www/html/client-ohp.ovpn
+auth-user-pass
+client
+dev tun
+proto tcp
+remote "Seve-Scripts" 443
+persist-key
+persist-tun
+pull
+resolv-retry infinite
+nobind
+user nobody
+comp-lzo
+remote-cert-tls server
+verb 3
+mute 2
+connect-retry 5 5
+connect-retry-max 8080
+mute-replay-warnings
+redirect-gateway def1
+script-security 2
+cipher none
+auth none
+http-proxy $het 45679
+<ca>
+-----BEGIN CERTIFICATE-----
+MIIDKzCCAhOgAwIBAgIJAMhZCGrFq5X/MA0GCSqGSIb3DQEBCwUAMBMxETAPBgNV
+BAMMCENoYW5nZU1lMB4XDTIwMDkyODIzMTIxMVoXDTMwMDkyNjIzMTIxMVowEzER
+MA8GA1UEAwwIQ2hhbmdlTWUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIB
+AQDJKh5ceRXC1322TJRXCeYWTxeyn8nWKVnfT1IC/oNMlpS9k1y77xEcdrJX/RSU
+9paz/3OrX39KmMZeGQDp9cl8on/+ZSrFtQkUafX5o9jONuhl2Yw/agkurLFwA8AY
+t98ehGdw4iocMawMhZUzYwiwB9JJ+OSzaymrdNF5qY+JEBYFL9joK5tCxQvl8M5H
+hbW0c+YN0wyt58kxPpVslhzpmGTDk2xmHdbS47H7W/g7Xw4W+ca7R1utXKPPZRg4
+gaT0fniBi6PZIAFEmjvnmTAW64gahucyBOA4qS/cj9TLtab85S+VARl0Gku86j2J
+TSdURN2kz9OCRJmWoR+papA/AgMBAAGjgYEwfzAdBgNVHQ4EFgQUquj49aauREaH
+oBdwg+dNRA+mDvMwQwYDVR0jBDwwOoAUquj49aauREaHoBdwg+dNRA+mDvOhF6QV
+MBMxETAPBgNVBAMMCENoYW5nZU1lggkAyFkIasWrlf8wDAYDVR0TBAUwAwEB/zAL
+BgNVHQ8EBAMCAQYwDQYJKoZIhvcNAQELBQADggEBADxqH/K0VhWfab9nP/tcZ7sC
+ZZuHY3iRsr74Igdx7oTR5xjWrE4FqmmJO/yg3dbWyY7HAw3cVC8+zdm1ffH03aJ3
+IYjiffCJdr9MSjvpLJdj+ijPT8gSO4qFwczpPhOgShqIu0xs7XJl03XuKCoEGiiD
+20BNXJBCllqDIpU8fRs2F7OFJcVf+4VgWljLbBF4rv3iGQdq9YJfnyjmpU22AMiJ
+TJm3B6wq+S0hMuYgIBHhx4DgLEEPoyvcW8mcpxktYvhVVCBmPp1Omx2k7qkXPsox
+at0RQ9kNzmuKkracloVPphekcEgHC+RhurGyYZumAC+jofod51ARMng6xv1tkLI=
+-----END CERTIFICATE-----
+</ca>
+EOF
+# Delete On SQUID
+sed -i '/Kangarif/d' /etc/squid/squid.conf
+cat <<EOF >>/etc/squid/squid.conf
+visible-hostname SEVE-SCRIPT
+EOF
+
+
+# INFO
+info_script(){ 
+				het="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+				echo "==========================================" 
+				echo " SCRIPT IS SUCCESSFULLY INSTALLED"
+				echo " SSH Port is 22, 225" 
+				echo " PROXY Port is 8000, 8080" 
+				echo " OPENVPN Port is 1194" 
+				echo " Config List:" 
+				echo " $het:81/client-tcp.ovpn"
+ 				echo " $het:81/client-udp.ovpn" 
+ 				echo " $het:81/client-ohp.ovpn"
+ 				echo " Webmin: $het:10000" 
+                                echo " Create Username: usernew"
+				echo 
+				echo "${green}===========AUTO SCRIPT BY SEVE===========${norm}" 
+				echo "=========================================="
+}
+# INFO SHOW
+info_script
