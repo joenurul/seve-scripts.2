23908d84c3d3c6b5523765c912c5a21be539bdf1
mathew1357 <58839700+mathew1357@users.noreply.github.com>
2020-10-14 21:53:35 +0800

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,5 +1,12 @@
+#ECHO COLORS
+red="`tput setaf 1`"
+green="`tput setaf 2`"
+cyan="`tput setaf 6`"
+bold="`tput bold`"
+norm="`tput sgr0`"
+magen="`tput setaf 5`"
 # Install
-wget -q https://github.com/lfasmpao/open-http-puncher/releases/download/0.1/ohpserver-linux32.zip
+wget https://github.com/lfasmpao/open-http-puncher/releases/download/0.1/ohpserver-linux32.zip
 # Unzip
 unzip ohpserver-linux32.zip
 # Run As Root
@@ -13,6 +20,10 @@ Description= SeveScripts
 Wants=network.target After=network.target
 [Service]
 ExecStart=/usr/local/bin/ohpserver -port 45678 -proxy 127.0.0.1:8000 -tunnel 127.0.0.1:143
+Restart=always
+RestartSec=3
+[Service]
+ExecStart=/usr/local/bin/ohpserver -port 45679 -proxy 127.0.0.1:8000 -tunnel 127.0.0.1:1194
 Restart=always
 RestartSec=3
 [Install]
@@ -30,8 +41,8 @@ start(){
 
 info_ohp(){ 
 				echo "==========================================" 
-				echo "  OHP IS SUCCESSFULLY STARTED"
-				echo "    Your Port is 45678" 
+				echo " OHP IS SUCCESSFULLY STARTED"
+				echo " Your Port is 45678" 
 				echo 
 				echo "${green}===========AUTO SCRIPT BY SEVE===========${norm}" 
 				echo "=========================================="
