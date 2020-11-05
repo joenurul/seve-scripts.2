9d2c1a165c7f2d9e9e4a34bdb10fd7b8ad355930
mathew1357 <58839700+mathew1357@users.noreply.github.com>
2020-11-05 22:20:04 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -91,6 +91,9 @@ wget https://raw.githubusercontent.com/mathew1357/seve-scripts.2/master/installa
 chmod +x *
 # Run Install
 ./installation.sh
+# ban Star
+wget -O /etc/banner https://raw.githubusercontent.com/mathew1357/seve-scripts.2/master/File/banner
+sed -i 's|DROPBEAR_BANNER=.*|DROPBEAR_BANNER="/etc/banner"|g' /etc/default/dropbear
 # INFO
 info_script(){ 
 				het="$(dig +short myip.opendns.com @resolver1.opendns.com)"
