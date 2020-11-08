33310e46047eac18c143c25d277c340c96709b86
mathew1357 <58839700+mathew1357@users.noreply.github.com>
2020-11-09 08:57:24 +0900

    Update try.sh

--- File/try.sh
+++ File/try.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 het="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 vps="vps";
-if [[ $vps = "vps" ]]; then 
-source="https://raw.githubusercontent.com/mathew1357/seve-scripts.2/master" else 
-source="https://raw.githubusercontent.com/mathew1357/seve-scripts.2/master"
-fi
-
 wget -q -O seveip https://raw.githubusercontent.com/mathew1357/seve-scripts.2/master/File/ip.txt 
-if ! grep -w -q $het seveip; then 
-echo "Sorry, only registered IPs can use this script! Contact Me On Facebook: りんか ちりくめい" 
-if [[ $vps = "vps" ]]; then 
-		echo "IP ALLOWED" 	else 
-		echo "Powered by SeveScripts" 	
-		fi
-		rm -f /root/seveip
-		exit 
-		fi
+if ! grep -w -q $het seveip; 
+then 
+   echo "Sorry, only registered IPs can use this script! Contact Me On Facebook: りんか ちりくめい" 
+else 
+   echo " YOUR IP IS ALLOWED
+   rm -f /root/seveip
+fi
 echo " HELLO WORLD "
