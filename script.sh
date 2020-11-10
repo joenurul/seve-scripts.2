5f2046eca8b8560af89d72fbc2d523bc429ce629
mathew1357 <58839700+mathew1357@users.noreply.github.com>
2020-11-10 17:45:32 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -25,16 +25,17 @@ else
    echo "${red}Access Denied!!${norm}"
    exit 1
 fi
-wget -q -O seveip https://raw.githubusercontent.com/mathew1357/seve-scripts.2/master/File/ip.txt 
-if ! grep -w -q $het seveip; 
+wget -O /etc/seveip https://raw.githubusercontent.com/mathew1357/seve-scripts.2/master/File/ip.txt 
+clear
+if ! grep -w -q $het /etc/seveip
 then 
    echo "${red}Sorry, only registered IPs can use this script! Contact Me On Facebook: りんか ちりくめい${norm}" 
-   rm -f /root/seveip
+   rm /etc/seveip
    exit 1
 else 
    echo
    echo "${green} WELCOME USER ${norm}"
-		rm -f /root/seveip
+   rm /etc/seveip
 fi
 # HELLO +GOOGLE
 read -n 1 -s -r -p "Press ${green}Enter Key${norm} to Install Or Press ${red}CTRL + C${norm} to stop"
