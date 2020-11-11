f0f0d0ea7c6196fc4c3d6e5d5861142ef4cf1617
mathew1357 <58839700+mathew1357@users.noreply.github.com>
2020-11-11 19:12:08 +0900

    Update scriptfixer.sh

--- scriptfixer.sh
+++ scriptfixer.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
+colors(){
 red="`tput setaf 1`"
 green="`tput setaf 2`"
 cyan="`tput setaf 6`"
 bold="`tput bold`"
 norm="`tput sgr0`"
 magen="`tput setaf 5`"
-
+}
+intro(){
+clear
+colors
+echo "   ░▒█▀▀▀█░▒█▀▀▀░▒█░░▒█░▒█▀▀▀"
+echo "   ░░▀▀▀▄▄░▒█▀▀▀░░▒█▒█░░▒█▀▀▀"
+echo "   ░▒█▄▄▄█░▒█▄▄▄░░░▀▄▀░░▒█▄▄▄ v2"
+echo
+echo "${magen} FIXER OF SEVE SCRIPTS UBUNTU & DEBIAN ${norm}"
+echo
+echo "    Can Fix: Dropbear, Auto Recon, Squid (OPTIONAL)"
+echo "       To Fix SQUID (UBUNTU ONLY): $0 ubunt"
+read -n 1 -s -r -p "Press ${green}Enter Key${norm} to Proceed Or Press ${red}CTRL + C${norm} to stop"
+}
+fix(){ 
+colors
 # Fix Dropbear
 sed -i '/DROPBEAR_EXTRA_ARG/d' /etc/default/dropbear | sed -i '8iDROPBEAR_EXTRA_ARGS="-p 80"' /etc/default/dropbear
 sudo systemctl restart dropbear
+# Fix Auto Recon
+sed -i '/^/d' /etc/systemd/system/autorecon.service
 cat <<EOF >>/etc/systemd/system/autorecon.service
 [Unit]
 Description= SeveScripts
@@ -26,8 +44,9 @@ EOF
 sudo systemctl daemon-reload
 sudo systemctl start autorecon 
 clear
+}
 # Squid Proxy Fixer
-ubunt(){
+ubunt(){ 
 sed -i '/^/d' /etc/squid/squid.conf
 cat <<EOF >>/etc/squid/squid.conf
 # In Partner Of ATSL
@@ -57,6 +76,20 @@ visible_hostname SEVE-SCRIPTS
 EOF
 sudo systemctl restart squid
 clear
-echo "${green}The SquidPackage Has FIXED!!${norm}"
-echo "Fixed By ATSL/SEVESCRIPTS"
 }
+#FINISHING
+intro
+fix
+
+
+action=$1
+[ -z $1 ] && action="$(exit 0)"
+case "$action" in 
+ubunt) 
+${action}
+;; 
+*) 
+echo "${red}Arguments error! [ ${action} ]${norm}" 
+echo "Usage: $0 [ubunt]" 
+;;
+esac
