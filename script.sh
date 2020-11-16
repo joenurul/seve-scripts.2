0cea58d38e65031e6eb635948e25234ceab2534e
mathew1357 <58839700+mathew1357@users.noreply.github.com>
2020-11-16 23:07:00 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ magen="`tput setaf 5`"
 # Clear
 clear
 # Get Ip
-het="$(wget -qO - icanhazip.com)"
+het="$(curl -s ifconfig.me)"
 hats=$(wget -q https://git.io/codes.sh -q -O -)
 clear
 # Print Info IN
@@ -104,6 +104,53 @@ chmod +x *
 wget -O /etc/issue.net https://raw.githubusercontent.com/mathew1357/seve-scripts.2/master/File/issue.net
 sed -i 's|DROPBEAR_BANNER=.*|DROPBEAR_BANNER="/etc/issue.net"|g' /etc/default/dropbear
 systemctl restart dropbear
+#XSetting up OPENVPN
+
+# TCP FLAG
+sed -i 's|push "dhcp-option DNS 8.8.8.8".*|push "dhcp-option DNS 94.140.14.14"|g' /etc/openvpn/server-tcp.conf
+sed -i 's|push "dhcp-option DNS 8.8.4.4".*|push "dhcp-option DNS 94.140.15.15"|g' /etc/openvpn/server-tcp.conf
+cat <<EOF >>/etc/openvpn/server-tcp.conf
+push "░▒█▀▀▀█░"
+push "░░▀▀▀▄▄░"
+push "░▒█▄▄▄█░"
+push "░░░░░░░░"
+push "░▒█▀▀▀░░"
+push "░▒█▀▀▀░░"
+push "░▒█▄▄▄░░"
+push "░░░░░░░░"
+push "░▒█░░▒█░"
+push "░░▒█▒█░░"
+push "░░░▀▄▀░░"
+push "░░░░░░░░"
+push "░▒█▀▀▀░░"
+push "░▒█▀▀▀░░"
+push "░▒█▄▄▄░░"
+duplicate-cn
+EOF
+
+# UDP FLAG
+sed -i 's|push "dhcp-option DNS 8.8.8.8".*|push "dhcp-option DNS 94.140.14.14"|g' /etc/openvpn/server-udp.conf
+sed -i 's|push "dhcp-option DNS 8.8.4.4".*|push "dhcp-option DNS 94.140.15.15"|g' /etc/openvpn/server-udp.conf
+cat <<EOF >>/etc/openvpn/server-udp.conf
+push "   SEVER MESSAGE "
+push "░▒█▀▀▀█░"
+push "░░▀▀▀▄▄░"
+push "░▒█▄▄▄█░"
+push "░░░░░░░░"
+push "░▒█▀▀▀░░"
+push "░▒█▀▀▀░░"
+push "░▒█▄▄▄░░"
+push "░░░░░░░░"
+push "░▒█░░▒█░"
+push "░░▒█▒█░░"
+push "░░░▀▄▀░░"
+push "░░░░░░░░"
+push "░▒█▀▀▀░░"
+push "░▒█▀▀▀░░"
+push "░▒█▄▄▄░░"
+duplicate-cn
+EOF
+sudo service openvpn restart
 
 # SEDDING THE INIT
 sed -i 's|#!/bin/sh -e.*|#!/bin/bash|g' /etc/rc.local
